8d027eaa512d9e3cb985f5c7263a247bf2425e1e
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2025-02-25 09:59:32 +0100

    feat: improve cols page

--- pages/cols.py
+++ pages/cols.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
+from src.code.main import Code
 from src.cols.main import Cols
 from src.component import Component
+from src.divider.main import Divider
 from src.header.main import Header
 from src.link.main import Link
+from src.tab.main import Tab
 from src.title.main import Title
 
 
@@ -15,6 +18,8 @@ class ColsPage(Component):
         header = Header("Cols Example")
         header.render()
 
+        divider = Divider()
+        tab = Tab()
         cols = Cols()
         title = Title("Title 1")
         cols.add(title)
@@ -22,7 +27,31 @@ class ColsPage(Component):
         cols.add(another_title)
         extra_title = Title("Title 3")
         cols.add(extra_title)
-        cols.render()
+
+        tab.add("Cols", cols)
+        lines = [
+            "cols = Cols()",
+            "\n",
+            'title = Title("Title 1")',
+            "cols.add(title)",
+            "\n",
+            'another_title = Title("Title 2")',
+            "cols.add(another_title)",
+            "\n",
+            'extra_title = Title("Title 3")',
+            "cols.add(extra_title)",
+            "\n",
+            "cols.add(extra_title)",
+        ]
+        content = "\n".join(lines)
+        code = Code(
+            language="python",
+            content=content,
+        )
+        tab.add("Source code", code)
+        tab.render()
+
+        divider.render()
 
         Link.go_home()
 
